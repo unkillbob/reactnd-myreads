784c1d72d4f1a3bff6d95f39aa74ed843d6d015e
James Bunt <james.bunt@vendhq.com>
2017-08-14 10:25:38 +1200

    Polyfill and mock BooksAPI to get basic render unit test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { MemoryRouter } from 'react-router'
+import App from './App'
+
+jest.mock('./BooksAPI', () => {
+  return {
+    getAll: jest.fn(() => Promise.resolve([]))
+  }
+})
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div)
+})
