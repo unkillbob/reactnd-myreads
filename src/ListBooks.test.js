782bf6ab6512cb79114c2cf0b2f9c89c89771dd8
James Bunt <james.bunt@vendhq.com>
2017-08-21 14:59:19 +1200

    Add more thorough tests for ListBooks component

--- src/ListBooks.test.js
+++ src/ListBooks.test.js
@@ -1,8 +1,52 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import { MemoryRouter } from 'react-router'
+import { mount, shallow } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
 import ListBooks from './ListBooks'
+import BookShelf from './BookShelf'
 
 it('renders without crashing', () => {
   mount(<MemoryRouter><ListBooks books={[]} /></MemoryRouter>)
+})
+
+describe('on render', () => {
+  const books = []
+  const onUpdate = jest.fn()
+  let wrapper
+
+  beforeAll(() => {
+    wrapper = shallow(<ListBooks books={books} onUpdateBookShelf={onUpdate} />)
+  })
+
+  it('displays a Currently Reading book shelf', () => {
+    expect(wrapper).toContainReact(
+      <BookShelf
+        title='Currently Reading'
+        shelf='currentlyReading'
+        books={books}
+        onUpdateBookShelf={onUpdate}
+      />
+    )
+  })
+
+  it('displays a Want to Read book shelf', () => {
+    expect(wrapper).toContainReact(
+      <BookShelf
+        title='Want to Read'
+        shelf='wantToRead'
+        books={books}
+        onUpdateBookShelf={onUpdate}
+      />
+    )
+  })
+
+  it('displays a Read book shelf', () => {
+    expect(wrapper).toContainReact(
+      <BookShelf
+        title='Read'
+        shelf='read'
+        books={books}
+        onUpdateBookShelf={onUpdate}
+      />
+    )
+  })
 })
